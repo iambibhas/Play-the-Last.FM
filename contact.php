ffb8e4cdb1c713a52ed67fca36af172bdd0576fc
iambibhas <iambibhas@gmail.com>
2010-08-14 04:39:19 +0530

    Spelling mistake..

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@
 			<h2>The Creator</h2>
             <div id="creator-desc">
                 The creator of this app is <b>Bibhas Chandra Debnath</b>.<br />
-                Bibhas is a engineering student, yet to complete graduation. He is a programmer and works on PHP and .NET framework mainly. More details on him can be found on his site <a target="_blank" href="http://bibhas.in">Bibhas.in</a> and his blog "<a target="_blank" href="http://bibhas.in/blog">Lazy Blogger</a>".<br />
+                Bibhas is an engineering student, yet to complete graduation. He is a programmer and works on PHP and .NET framework mainly. More details on him can be found on his site <a target="_blank" href="http://bibhas.in">Bibhas.in</a> and his blog "<a target="_blank" href="http://bibhas.in/blog">Lazy Blogger</a>".<br />
             </div>
 		</div>
 		<!-- end #content -->
