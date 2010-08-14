08de56b861fbd55164d70c4beab0b0b2eb72148e
iambibhas <iambibhas@gmail.com>
2010-08-15 02:29:27 +0530

    Fixed facebook page link

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
 		<div id="content">
 			<h1>play the <img src="images/lastfm-logo1.png" style="border:none;" /></h1>
             <div id="about-content">
-            <i>play the last.fm</i> is an open source <a target="_blank" href="http://last.fm">Last.fm</a> web app made in PHP. The sole reason of its creation is a coding competition "<a target="_blank" href="http://www.facebook.com/album.php?profile=1&id=142473969104556">Code to Win</a>", organised by some innovative people to encourage open source development.<br /><br />The source of this app can be found at <a target="_blank" href="http://github.com/iambibhas/Play-the-Last.FM">Github repository</a>.
+            <i>play the last.fm</i> is an open source <a target="_blank" href="http://last.fm">Last.fm</a> web app made in PHP. The sole reason of its creation is a coding competition "<a target="_blank" href="httphttp://www.facebook.com/pages/Code-to-win/142473969104556">Code to Win</a>", organised by some innovative people to encourage open source development.<br /><br />The source of this app can be found at <a target="_blank" href="http://github.com/iambibhas/Play-the-Last.FM">Github repository</a>.
             </div>
 		</div>
 		<!-- end #content -->
